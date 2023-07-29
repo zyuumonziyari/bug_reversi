0f95e1ed1038ea35520e7dd384e116dcb07180ff
beta_chelsea <beta.chelsea@gmail.com>
2023-07-29 10:48:53 +0900

    Use `assert x`, `refute x`

--- test/test.rb
+++ test/test.rb
@@ -34,30 +34,30 @@ class TestReversi < Minitest::Test
 
   def test_put_stone
     board = build_board('pattern00.txt')
-    assert_equal true, put_stone!(board, 'e3', BLACK_STONE)
+    assert put_stone!(board, 'e3', BLACK_STONE)
     assert_equal build_board('pattern00_step1.txt'), board
-    assert_equal true, put_stone!(board, 'f5', WHITE_STONE)
+    assert put_stone!(board, 'f5', WHITE_STONE)
     assert_equal build_board('pattern00_step2.txt'), board
   end
 
   def test_cannot_put_stone
     board = build_board('pattern01.txt')
-    assert_equal false, put_stone!(board, 'b8', BLACK_STONE)
+    refute put_stone!(board, 'b8', BLACK_STONE)
     assert_equal build_board('pattern01.txt'), board
   end
 
   def test_turn
     board = build_board('pattern02.txt')
-    assert_equal true, put_stone!(board, 'b5', BLACK_STONE)
+    assert put_stone!(board, 'b5', BLACK_STONE)
     assert_equal build_board('pattern02_after.txt'), board
   end
 
   def test_finished?
-    assert_equal false, finished?(build_board('pattern00.txt'))  # 初期盤面
-    assert_equal true,  finished?(build_board('pattern03a.txt')) # 全て埋まった
-    assert_equal true,  finished?(build_board('pattern03b.txt')) # 白最短勝利
-    assert_equal true,  finished?(build_board('pattern03c.txt')) # 黒最短勝利
-    assert_equal false, finished?(build_board('pattern03d.txt')) # 白配置可・黒配置不可
-    assert_equal false, finished?(build_board('pattern03e.txt')) # 白配置不可・黒配置可
+    refute finished?(build_board('pattern00.txt'))  # 初期盤面
+    assert finished?(build_board('pattern03a.txt')) # 全て埋まった
+    assert finished?(build_board('pattern03b.txt')) # 白最短勝利
+    assert finished?(build_board('pattern03c.txt')) # 黒最短勝利
+    refute finished?(build_board('pattern03d.txt')) # 白配置可・黒配置不可
+    refute finished?(build_board('pattern03e.txt')) # 白配置不可・黒配置可
   end
 end
